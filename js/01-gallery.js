dcb2b4a55c6b010f6cd2cc09ba5367811d010830
romanfliak <rfliak568@gmail.com>
2023-07-12 18:17:53 +0200

    .

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,24 @@
+
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
-
 console.log(galleryItems);
+const collection = document.querySelector('.gallery') 
+    const listImg = galleryItems.map(({}) => 
+   
+  <li class="gallery-item">
+ <a href="${original}">      
+     <img
+     class ="gallery-items" 
+     src="${preview}" 
+     data-sourse="${original}"
+     alt="${description    }"/>
+ </a>
+</li> 
+ ).join('')
+
+
+
+ // list.insertAdjacentHTML('afterBegin', listImg )
+
+
+
